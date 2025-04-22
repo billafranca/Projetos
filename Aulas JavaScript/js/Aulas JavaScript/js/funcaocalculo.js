0cadcffc7946e3ac418b302bff7ef58dbb9a2de3
Pedro <bragap379@gmail.com>
2025-04-22 07:52:50 -0300

    Add files via upload

---
+++ Aulas JavaScript/js/Aulas JavaScript/js/funcaocalculo.js
@@ -0,0 +1,8 @@
+const n1 = parseFloat(prompt("value of n1: "));
+const m2 = parseFloat(prompt("value of n2: "))
+
+soma = n1+n2
+function soma(){
+    const result = n1+n2
+    console.log("result = "+result)
+}
---
+++ Aulas JavaScript/js/Aulas JavaScript/js/todolist.js
@@ -0,0 +1,5 @@
+const input = document.querySelector('input');
+const botao = document.querySelector('button');
+
+const lista = document.querySelector('ul');
+
