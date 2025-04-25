b28ba9bf2d7a2b9b259412fc1b32e126283b9665
Pedro <bragap379@gmail.com>
2025-04-24 22:08:19 -0300

    Add files via upload

---
+++ Java codes/Screen.java
@@ -0,0 +1,45 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+public class Screen extends JFrame{
+
+    JTextField num1;
+    JTextField num2;
+    public Screen() {
+        setVisible(true);
+        setSize(700,700);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+        setLayout(null);
+
+        JButton jbutton = new JButton("sum");
+        jbutton.setBounds(250, 250, 100, 100);
+        jbutton.setBackground(new Color(122, 255, 220));
+        add(jbutton);
+        jbutton.addActionListener(this::somar);
+        Font font = new Font("Arial Black", Font.BOLD, 10);
+
+        num1 = new JTextField("number one: ");
+        num1.setBounds(100, 100, 100, 100);
+        num1.setFont(font);
+        num2 = new JTextField("number two: ");
+        num2.setBounds(200, 100, 100, 100);
+        num2.setFont(font);
+        add(num1);
+        add(num2);
+
+
+    }
+    private void somar(ActionEvent ae){
+        try{
+            int numero1 = Integer.parseInt(num1.getText());
+            int numero2 = Integer.parseInt(num2.getText());
+            int soma = numero1 + numero2;
+            JOptionPane.showMessageDialog(null, "the sum is: "+ soma, "soma",JOptionPane.INFORMATION_MESSAGE);
+
+        }catch(NumberFormatException e) {
+            JOptionPane.showMessageDialog(null, "error", "error", JOptionPane.ERROR_MESSAGE);
+        }
+
+    }
+}
