d763d12fb69dbaf1ea06062343bc6fb2f12caf05
Pedro <bragap379@gmail.com>
2025-04-24 22:25:33 -0300

    Add files via upload

--- Java codes/Screen.java
+++ Java codes/Screen.java
@@ -1,23 +1,34 @@
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
-public class Screen extends JFrame{
+public class Screen extends JFrame {
 
     JTextField num1;
     JTextField num2;
+
     public Screen() {
         setVisible(true);
-        setSize(700,700);
+        setSize(700, 700);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLocationRelativeTo(null);
         setLayout(null);
 
-        JButton jbutton = new JButton("sum");
+        JButton jbutton = new JButton("sum/sub");
         jbutton.setBounds(250, 250, 100, 100);
         jbutton.setBackground(new Color(122, 255, 220));
         add(jbutton);
         jbutton.addActionListener(this::somar);
         Font font = new Font("Arial Black", Font.BOLD, 10);
+
+
+        JButton jb = new JButton("subtraction");
+        jbutton.setBounds(350, 250, 100, 100);
+        jbutton.setBackground(new Color(122, 255, 220));
+        add(jbutton);
+        jbutton.addActionListener(this::sub);
+        add(jbutton);
+        Font ft = new Font("Arial Black", Font.BOLD, 10);
+
 
         num1 = new JTextField("number one: ");
         num1.setBounds(100, 100, 100, 100);
@@ -30,16 +41,32 @@ public class Screen extends JFrame{
 
 
     }
-    private void somar(ActionEvent ae){
-        try{
+
+    private void somar(ActionEvent ae) {
+        try {
             int numero1 = Integer.parseInt(num1.getText());
             int numero2 = Integer.parseInt(num2.getText());
             int soma = numero1 + numero2;
-            JOptionPane.showMessageDialog(null, "the sum is: "+ soma, "soma",JOptionPane.INFORMATION_MESSAGE);
 
-        }catch(NumberFormatException e) {
+            JOptionPane.showMessageDialog(null, "the sum is: " + soma, "soma", JOptionPane.INFORMATION_MESSAGE);
+
+        } catch (NumberFormatException e) {
             JOptionPane.showMessageDialog(null, "error", "error", JOptionPane.ERROR_MESSAGE);
         }
 
+
+    }
+
+    private void sub(ActionEvent actionEvent) {
+        try {
+            int numero1 = Integer.parseInt(num1.getText());
+            int numero2 = Integer.parseInt(num2.getText());
+            int subtraction = numero1 - numero2;
+
+            JOptionPane.showMessageDialog(null, "the sub is: " + subtraction, "subtraction", JOptionPane.INFORMATION_MESSAGE);
+
+        } catch (NumberFormatException e) {
+            JOptionPane.showMessageDialog(null, "error", "error", JOptionPane.ERROR_MESSAGE);
+        }
     }
 }
