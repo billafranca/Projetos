57aece701f9906bdc468e5f5c020d74e46b03f04
Pedro <bragap379@gmail.com>
2025-04-30 08:14:29 -0300

    Add files via upload

--- WebSites -PW/js/script2.js
+++ WebSites -PW/js/script2.js
@@ -2,15 +2,17 @@ document.addEventListener("DOMContentLoaded", function() {
     document.getElementById("enviar").addEventListener("click", function() {
         var nota = document.getElementById("nota").value;
         
-        // Converte o valor para número
+        
         nota = parseFloat(nota);
         
-        if (nota < 5) {
-            alert("que pena, mas obrigado!");
-        } else if (nota <=7){
-            alert("bom, obrigado!");
+        if (nota < 0 || nota > 10) {
+            alert("Por favor, insira um número válido entre 0 e 10.");
+        } else if (nota <= 5) {
+            alert("Que pena, mas obrigado!");
+        } else if (nota <= 7) {
+            alert("Bom, obrigado!");
+        } else {
+            alert("Muito obrigado!");
         }
-        else
-            alert("muito obrigado!");
     });
 });
