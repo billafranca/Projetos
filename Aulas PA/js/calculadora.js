b8152c245e03e41926c0fad7a6a7ce747480dfca
Pedro <bragap379@gmail.com>
2025-05-08 13:29:43 -0300

    calculadora

--- Aulas PA/js/calculadora.js
+++ Aulas PA/js/calculadora.js
@@ -1,10 +1,35 @@
-var expression = prompt("Digite a expressão (ex: 3 + 4): ");
-try {
-    var result = eval(expression);
-    if (isNaN(result)) {
-        throw "Operação inválida";
-    }
-    alert(result);
-} catch (e) {
-    alert("Operação inválida");
-}
+    document.addEventListener("DOMContentLoaded", function(){
+    let number1 = document.getElementById("number1");
+    let number2 = document.getElementById("number2");
+
+    const somar = document.getElementById("somar");
+    const subtrair = document.getElementById("subtrair");
+    const dividir = document.getElementById("dividir");
+    const multiplicar = document.getElementById("multiplicar");
+
+    const resultadoElement = document.getElementById("resultado");
+
+    somar.addEventListener("click", function(){
+        const resultado = Number(number1.value) + Number(number2.value);
+        resultadoElement.textContent = `O resultado: ${resultado}`;
+    });
+
+    subtrair.addEventListener("click", function(){
+        const resultado = Number(number1.value) - Number(number2.value);
+        resultadoElement.textContent = `O resultado: ${resultado}`;
+    });
+
+    dividir.addEventListener("click", function(){
+        if(Number(number2.value) === 0) {
+            alert("Não é possível dividir por zero.");
+        } else {
+            const resultado = Number(number1.value) / Number(number2.value);
+            resultadoElement.textContent = `O resultado: ${resultado}`;
+        }
+    });
+
+    multiplicar.addEventListener("click", function(){
+        const resultado = Number(number1.value) * Number(number2.value);
+        resultadoElement.textContent = `O resultado: ${resultado}`;
+    });
+});
