ab1163082144aab1886d7fbf32d5cafeb1f482e0
PEDRO BRAGA BILLAFRANCA <00001118653804sp@aluno.educacao.sp.gov.br>
2025-05-12 07:28:27 -0300

    alterações

---
+++ Projetos_Pessoais/e-commerce ws/script.js
@@ -0,0 +1,34 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const comp_preco = 2300;
+    const tv_preco = 1200;
+    const add_com = document.getElementById("add1");
+    const add_tv = document.getElementById("add2");
+    const car = document.getElementById("car");
+    let amt_com = 0;
+    let amt_tv = 0;
+    let carrinho = 0.00;
+
+    add_com.addEventListener("click", function() {
+        alert("Computador adicionado ao carinho");
+        carrinho += comp_preco;
+        amt_com += 1;
+        if (amt_tv == 0) {
+            document.getElementById("comprados").textContent = amt_com + "x Computadores";
+        } else {
+            document.getElementById("comprados").textContent = amt_com + "x Computadores\n" + amt_tv + "x Televisões";
+        }
+        car.textContent = "$" + carrinho;
+    })
+
+    add_tv.addEventListener("click", function() {
+        alert("Televisão adicionado ao carinho");
+        carrinho += tv_preco;
+        amt_tv += 1;
+        if (amt_com == 0) {
+            document.getElementById("comprados").textContent = amt_tv + "x Televisões";
+        } else {
+            document.getElementById("comprados").textContent = amt_com + "x Computadores\n" + amt_tv + "x Televisões";
+        }
+        car.textContent = "$" + carrinho;
+    })
+});
