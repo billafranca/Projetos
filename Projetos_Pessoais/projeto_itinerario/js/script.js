6a39f2fd27427be6c98767ed8fc7232e2c1223cb
PEDRO_5245 <PEDRO_5245@uirapuru.br>
2025-05-22 19:03:14 -0300

    alteração projetos pessoais

---
+++ Projetos_Pessoais/projeto_itinerario/js/script.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function(){
+    var cnt = document.getElementById("contato");
+
+    cnt.addEventListener("click", function(){
+        alert("O contato é:\n Telefone: (11) 9999-9999 \n Email: Guiomarescola@gmail.com")
+    })
+})
