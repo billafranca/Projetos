c77b45fc4accd9cb64cf21fdcd9109d2fb2d21be
PEDRO_5245 <PEDRO_5245@uirapuru.br>
2025-05-30 20:21:29 -0300

    java projetos

---
+++ Projetos_Pessoais/java POO/alunos/Aluno.java
@@ -0,0 +1,12 @@
+public class Aluno {
+	String nome;
+	String cpf;
+	String rg;
+	String endereco;
+	double media;
+	static int nAls = 0;
+
+	public void add() {
+		nAls++;
+	}
+}
---
+++ Projetos_Pessoais/java POO/alunos/Main.java
@@ -0,0 +1,34 @@
+public class Main{
+	public static void main(String[] args){
+	Aluno al1 = new Aluno();
+	al1.nome = "josias delgas";
+	al1.cpf = "1234567890";
+	al1.rg = "1234567890";
+	al1.endereco = "rua da rua n-55";
+	al1.media = 9;
+	al1.add();
+
+	System.out.println(al1.nome);
+	System.out.println(al1.cpf);
+	System.out.println(al1.rg); 
+	System.out.println(al1.endereco);
+	System.out.println(al1.media);
+
+	Aluno al2 = new Aluno();
+	al2.nome = "maria da silva";
+	al2.cpf = "12648486";
+	al2.rg = "1234567890";
+	al2.endereco = "rua da rua n-65";
+	al2.media = 4;
+	al2.add();
+
+	System.out.println(al2.nome);
+	System.out.println(al2.cpf);
+	System.out.println(al2.rg); 
+	System.out.println(al2.endereco);
+	System.out.println(al2.media);
+
+	System.out.println("Total de alunos: " + Aluno.nAls);
+}
+}
+	 
